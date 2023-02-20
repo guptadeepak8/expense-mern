7ad11ee5289de291ae4774b0ffdcaf277b6ce773
guptadeepak8 <vectord7738@gmail.com>
2023-02-21 00:05:11 +0530

    updated

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -10,7 +10,7 @@ import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import { Link,useNavigate,useRegister } from 'react-router-dom';
+import { Link,useNavigate} from 'react-router-dom';
 import Cookies from 'js-cookie';
 
 
