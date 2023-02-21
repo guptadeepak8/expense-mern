330fac2ec6cf68bf5852c7d63a0d5f2359f65875
guptadeepak8 <vectord7738@gmail.com>
2023-02-21 23:16:10 +0530

    updated schema

--- server/models/Transaction.js
+++ server/models/Transaction.js
@@ -1,13 +1,12 @@
 import mongoose from "mongoose";
 import { Schema } from "mongoose";
 
-const transactionSchema=new Schema({
-  amount:Number,
-  text:String,
-  user_id:mongoose.Types.ObjectId,
-  date:{type:Date,default:new Date()},
-  createdAt:{type:Date,default:Date.now}
-})
+const transactionSchema = new Schema({
+  amount: { type:Number, required: true },
+  text: { type:String, required: true },
+  user_id: mongoose.Types.ObjectId,
+  date: { type: Date, default: new Date() },
+  createdAt: { type: Date, default: Date.now },
+});
 
-
-export default new mongoose.model("Transaction",transactionSchema);
+export default new mongoose.model("Transaction", transactionSchema);
