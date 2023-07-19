47ae685e09005d86a6c853a7490694ddd92f465b
guptadeepak8 <vectord7738@gmail.com>
2023-07-19 16:37:38 +0530

    updated ui

--- clients/src/pages/FormPage.jsx
+++ clients/src/pages/FormPage.jsx
@@ -83,71 +83,74 @@ export default function FormPage() {
   };
 
   const cancelEvent = () => {
-    setForm(InitalValue);
     setToggle(false);
     navigate("/");
   };
 
   return (
     <>
-      <div className="flex flex-col  ">
-        <div className="flex 	items-center mx-10 my-5">
-          <h2 className=" text-xl text-slate-600 font-bold">
-            {toggle ? "Update" : "Add New"} Transaction
-          </h2>
-          <button
-            className="ml-auto px-4  py-2  text-white  text-xl bg-slate-600 shadow-lg shadow-neutral-500/50  rounded-xl"
-            onClick={cancelEvent}
-          >
-            BACK
-          </button>
-        </div>
-        <form onSubmit={handleSubmit} className="flex flex-col  ">
-          <input
-            className="text-slate-600 min-[850px]:mx-50  outline-none text-xl  px-4 py-3 mx-10 my-5 shadow-lg shadow-indigo-700/50 rounded-3xl"
-            name="amount"
-            type="text"
-            placeholder="Amount"
-            onChange={handleChange}
-            value={form.amount}
-            required
-          />
-          <input
-            className="text-slate-600  text-xl outline-none px-4 py-3 mx-10 my-5 shadow-lg shadow-indigo-700/50 rounded-3xl"
-            name="text"
-            label="Text"
-            type="text"
-            placeholder="Text"
-            onChange={handleChange}
-            value={form.text}
-            required
-          />
-          <input
-            name="date"
-            className="text-slate-600  text-xl outline-none  px-4 py-3 mx-10 my-5 shadow-lg shadow-indigo-700/50 rounded-3xl"
-            type="date"
-            onChange={handleChange}
-            value={form.date ? form.date.slice(0, 10) : ""}
-            required
-          />
+      <div className="flex flex-col mt-12 items-center min-h-screen  ">
+        <div className="bg-zinc-100 rounded-xl">
+          <div className="py-6 px-10">
+            <div className="flex 	items-center justify-between my-3 ">
+              <h2 className=" text-xl text-slate-600 font-bold mr-2">
+                {toggle ? "Update" : "Add New"} Transaction
+              </h2>
+              <button
+                className="ml-auto px-4  py-2  text-white  text-xl bg-slate-600 shadow-lg shadow-neutral-500/50  rounded-xl"
+                onClick={cancelEvent}
+              >
+                BACK
+              </button>
+            </div>
+            <form onSubmit={handleSubmit} className="flex flex-col my-2 ">
+              <input
+                className="text-slate-600 min-[850px]:mx-50  outline-none text-xl  px-4 py-3 mx-3 my-3 shadow-lg shadow-indigo-700/50 rounded-3xl"
+                name="amount"
+                type="text"
+                placeholder="Amount"
+                onChange={handleChange}
+                value={form.amount}
+                required
+              />
+              <input
+                className="text-slate-600  text-xl outline-none px-4 py-3 mx-3 my-3 shadow-lg shadow-indigo-700/50 rounded-3xl"
+                name="text"
+                label="Text"
+                type="text"
+                placeholder="Text"
+                onChange={handleChange}
+                value={form.text}
+                required
+              />
+              <input
+                name="date"
+                className="text-slate-600  text-xl outline-none  px-4 py-3 mx-3 my-3 shadow-lg shadow-indigo-700/50 rounded-3xl"
+                type="date"
+                onChange={handleChange}
+                value={form.date ? form.date.slice(0, 10) : ""}
+                required
+              />
 
-          <div className="mx-10">
-            <button className=" mx-2 px-4  py-2  text-white  text-xl bg-slate-600 shadow-lg shadow-neutral-500/50  rounded-xl">
-              {loading ? (
-                <ColorRing
-                  color="#ffffff"
-                  height="50"
-                  width="50"
-                  className="py-2 px-4"
-                />
-              ) : toggle ? (
-                `UPDATE`
-              ) : (
-                "SUBMIT"
-              )}
-            </button>
+              <div className="mx-3 my-2">
+                <button className=" mx-2 px-4  py-2  text-white  text-xl bg-slate-600 shadow-lg shadow-neutral-500/50  rounded-xl">
+                  {loading ? (
+                    <ColorRing
+                      color="#ffffff"
+                      height="50"
+                      width="50"
+                      className="py-2 px-4"
+                    />
+                  ) : toggle ? (
+                    `UPDATE`
+                  ) : (
+                    "SUBMIT"
+                  )}
+                </button>
+              </div>
+            </form>
           </div>
-        </form>
+        </div>
       </div>
     </>
   );
